72808de4d5ac38d2f3f34d36af66311d4ab59e4d
aalicki <aalicki@gmail.com>
2016-06-21 20:55:57 -0400

    Adding in fixes to the Vagrant file and provisioning script.
    Adding in base API folder structure and base files.

---
+++ api/src/ToDo/Controllers/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ToDo\Controllers;
+
+class Main {
+
+	protected $core;
+
+	public function __construct() {
+		global $app;
+		$this->app = $app;
+	}
+
+	/*
+	 * If you request  local.todolist.com:8008/api
+	 * this is what shows up
+	 */
+	public function Index (){
+
+		echo 'the API routes and documentation';
+	}
+
+}
